9e354d262eaf968533db6553db2ffa39d9fc911b
stonekite <feelsknight@gmail.com>
2023-03-30 15:48:20 +0300

    added responsive classes to hero banner

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import RaidProgression from "./components/RaidProgression";
 
 const App = () => {
   return (
-    <div className="App font-nunito h-full bg-purple text-gold">
+    <div className="App font-nunito h-full bg-purple text-gold overflow-x-hidden">
       <Nav />
       <main className="max-w-5xl mx-auto flex flex-col gap-y-8 pb-8 pt-24">
         <HeroBanner />
